ca124c10d0cf3f42c7850b5a39eb86ee679dc682
Mouse Reeve <mousereeve@riseup.net>
2016-12-12 14:02:32 -0800

    Use both pronouns

--- birdbot.py
+++ birdbot.py
@@ -13,7 +13,7 @@ from urllib import urlretrieve
 # pick a bird form the list
 line = json.loads(random.choice(open('birdlist').readlines()))
 url = line['url']
-bird = line['name']
+bird = line['name'][0].lower() + line['name'][1:]
 
 # download the bird's wikipedia entry
 entry = requests.get('http://en.wikipedia.org%s' % url)
@@ -27,44 +27,48 @@ src = soup.find(class_='image').find('img')['src']
 extension = '.JPG' if '.JPG' in src else '.jpg'
 src = src.split(extension)[0] + extension
 image_url = 'https:' + re.sub('thumb/', '', src)
+
 # download bird image
 filename = re.sub(' ', '-', bird.lower()) + '.jpg'
 outpath = os.getcwd() + '/images/%s' % filename
 urlretrieve(image_url, outpath)
 
 # search twitter for the fact
+pronouns = ['she ', 'he ']
 prompts = [
-    'she acts like ',
-    'she doesn\'t ',
-    'she has ',
-    'she finds ',
-    'she is just ',
-    'she is only ',
-    'she is prone ',
-    'she keeps ',
-    'she loves to ',
-    'she lives in ',
-    'she makes ',
-    'she should really ',
-    'she totally ',
-    'she takes ',
-    'she thinks ',
-    'she travels ',
-    'she tries to ',
-    'she tends to ',
-    'she was ',
-    'she wasn\'t ',
-    'she will ',
+    'acts like ',
+    'doesn\'t ',
+    'has ',
+    'finds ',
+    'is just ',
+    'is only ',
+    'is prone ',
+    'keeps ',
+    'loves to ',
+    'lives in ',
+    'makes ',
+    'should really ',
+    'totally ',
+    'takes ',
+    'thinks ',
+    'travels ',
+    'tries to ',
+    'tends to ',
+    'was ',
+    'wasn\'t ',
+    'will ',
 ]
+
 # https://twitter.com/search?f=tweets&vertical=default&q=%22tends%20to%22&src=typd
-prompt = random.choice(prompts)
+pronoun = random.choice(pronouns)
+prompt = pronoun + random.choice(prompts)
 twitter_url = 'https://twitter.com/search?f=tweets&vertical=default&q="'
 twitter_url += prompt
 twitter_url += '"&src=typd'
 tweets = requests.get(twitter_url)
 soup = BeautifulSoup(tweets.text)
 
-fact = 'The %s (%s) %s' % (bird, scientific, re.sub('she ', '', prompt))
+fact = 'The %s (%s) %s' % (bird, scientific, re.sub(pronoun, '', prompt))
 
 # look through tweets
 for tweet in soup.find_all(class_='tweet-text'):
@@ -76,7 +80,7 @@ for tweet in soup.find_all(class_='tweet-text'):
     text = re.sub(r'([\.?!\n\r]).*$', r'\g<1>', text)
     if blacklist.check_blacklist(text):
         continue
-    if not re.search(r'["@)\|]|http|\bshe\b|\bher\b', text) \
+    if not re.search(r'["@)\|]|http|\bshe\b|\bher\b|\bhe\b|\bhim\b', text) \
             and len(fact + text) < 140:
         fact += text
         break
